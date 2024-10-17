8a99d1652b7df8ec7007fb0998075e8100805514
Heinriches <heydrich.institut@gmail.com>
2024-10-17 15:22:25 -0300

    Lesson 13: Final commit.

---
+++ Part 13 - FOR LOOPS (Exercises)/src/Exercise_02.kt
@@ -0,0 +1,16 @@
+//  FOR LOOP - Part 13
+
+//  Harder exercise:
+//  Create a program that asks the user for five numbers, and the calculates and
+//  displays the value of the average of those five numbers.
+
+fun main(){
+
+    println("Please type the value of FIVE numbers:")
+    var meme
+
+    for(i in 0..4){
+            readLine()?.toInt()
+    }
+    readln()
+}
