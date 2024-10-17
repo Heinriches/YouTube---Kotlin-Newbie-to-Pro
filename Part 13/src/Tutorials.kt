ab85a5f73ba65df106d68ffc8f3c5bc3ca1377b8
Heinriches <heydrich.institut@gmail.com>
2024-10-17 14:01:37 -0300

    Following video-tutorial

--- Part 13/src/Tutorials.kt
+++ Part 13/src/Tutorials.kt
@@ -1,5 +1,16 @@
 //  FOR-LOOP - Part 13
 
 fun main(){
+    val myArray = arrayOf(1, 2, 3)
+    for (i in myArray){
+        println(i)
+    }
 
+    for (x in 4..6){
+        println(x)
+    }
+
+    for (y  in 6 downTo 1){
+        println(y)
+    }
 }
